89f5b742db7249b5b47a5c00d149e8decb53c38e
darango91 <crash.diego91@gmail.com>
2019-09-11 13:41:45 -0500

    Testing URLs on django

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -19,5 +19,7 @@ from platzigram import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-world', views.hello_world),
+    path('hello-world/', views.hello_world),
+    path('sorted/', views.sorted_numbers),
+    path('hi/<str:name>/<int:age>', views.validate_age),
 ]
