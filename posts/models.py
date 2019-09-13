4d802a7fc68a090dd1c9e206db21b9f59fc78bb7
darango91 <crash.diego91@gmail.com>
2019-09-13 14:36:32 -0500

    playing arround with the django ORM

--- posts/models.py
+++ posts/models.py
@@ -7,15 +7,20 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     """User Model"""
-    id = models.UUIDField(primary_key=True)
-
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    is_admin = models.BooleanField(default=False)
 
     bio = models.TextField(blank=True)
 
     birthdate = models.DateField(blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """ String representation of the User model """
+        return self.email
