59489531c95268ea015ae1ae5e141ee9b86b3f88
darango91 <crash.diego91@gmail.com>
2019-09-13 17:46:03 -0500

    Adding users profile to django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,72 @@
+""" User admin classes. """
+# django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+# models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Profile admin."""
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('phone_number',)
+
+    search_fields = (
+        'user__username',
+        'user__email',
+        'user__first_name',
+        'user__last_name',
+        'phone_number'
+    )
+
+    list_filter = (
+        'created',
+        'modified',
+        'user__is_active',
+        'user__is_staff'
+    )
+
+    fieldsets = (
+        ('Profile', {
+            'fields': ('user', 'picture')
+        }),
+        ('Extra info', {
+            'fields': (
+                ('website', 'phone_number'),
+                'biography'
+            )
+        }),
+        ('Metadata', {
+            'fields': ('created', 'modified')
+        })
+    )
+
+    readonly_fields = ('created', 'modified')
+
+
+class ProfileInline(admin.StackedInline):
+    """Profile in-line admin for users."""
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
