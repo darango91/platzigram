77c971a2167d6e5332f54610cc382611d1a0c449
darango91 <crash.diego91@gmail.com>
2019-09-16 18:45:02 -0500

    Connecting the feed template to the actual db

--- posts/views.py
+++ posts/views.py
@@ -4,46 +4,18 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-# Utilities
-from datetime import datetime
+# Models
+from posts.models import Post
 
 # Forms
 from posts.forms import PostForm
 
-posts = [
-    {
-        'title': 'Mont Blanc',
-        'user': {
-            'name': 'Yessica Cortes',
-            'picture': 'https://picsum.photos/id/1027/60/60',
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/id/1036/800/600'
-    },
-    {
-        'title': 'Via Lactea',
-        'user': {
-            'name': 'C. Vander',
-            'picture': 'https://picsum.photos/id/1005/60/60',
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/id/903/800/600',
-    },
-    {
-        'title': 'Nuevo Auditorio',
-        'user': {
-            'name': 'Thepianistartist',
-            'picture': 'https://picsum.photos/id/883/60/60',
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/id/1076/800/600',
-    },
-]
 
 # Create your views here.
 @login_required()
 def list_posts(request):
     """List existing posts."""
+    posts = Post.objects.all().order_by('-created')
     return render(request, 'posts/feed.html', {'posts': posts})
 
 
