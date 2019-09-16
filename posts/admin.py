8c75f3fc2eb0f8e63d5125c175bc0d62425367d0
darango91 <crash.diego91@gmail.com>
2019-09-16 18:24:14 -0500

    Added posts to django admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
+"""Posts admin classes"""
+
+# Admin
 from django.contrib import admin
 
-# Register your models here.
+# models
+from django.contrib.auth.models import User
+from posts.models import Post
+
+
+@admin.register(Post)
+class PostsAdmin(admin.ModelAdmin):
+    """Posts admin."""
+    list_display = ('pk', 'user', 'title', 'photo')
